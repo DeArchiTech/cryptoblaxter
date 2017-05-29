e58d406fca4423636301b1234f75fe9da3dce0f1
David Chan <davidkwokhochan@gmail.com>
2017-05-28 22:51:49 -0700

    Added Coin base client code

--- coinExternalDataApi.py
+++ coinExternalDataApi.py
@@ -1,18 +1,33 @@
 import unittest
 import constants
-from coinbase.wallet.client import Client
+import json
 
-def fetchData():
-    return "To Be Implemented"
+from coinbase.wallet.client import Client
 
 def connectClient():
     return Client(constants.api_key, constants.api_secret)
 
+# Get Accounts
+def getUserAccounts():
+    client = connectClient()
+    return client.get_accounts()
+
+# Get User Info
+def getUserInfo():
+    client = connectClient()
+    user = client.get_current_user()
+    return json.dumps(user)
+
 # Here's our "unit tests".
 class functionTests(unittest.TestCase):
 
-    def testFetchData(self):
-        print(fetchData())
+    def testConnectClient(self):
+        connectClient()
+        print("ToBeImplemented")
+
+    def testGetUserInfo(self):
+        getUserInfo()
+        print("ToBeImplemented")
 
 def main():
     unittest.main()
