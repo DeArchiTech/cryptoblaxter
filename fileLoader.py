8d1c1139477d4d25cfa90f61c024c70a152bbc47
David Chan <davidkwokhochan@gmail.com>
2017-05-31 21:20:11 -0700

    Added X Y axis and title to plot

--- fileLoader.py
+++ fileLoader.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#df_bitcoinity = pd.read_csv('data/bitcoinity_data.csv')
-df_coindesk = pd.read_csv('data/coindesk-ETH-close_data-2015-08-31_2017-05-22.csv')
-#df_bitcoinity.plot(style=['o','rx'])
-df_coindesk.plot(style=['o','rx'])
-plt.show()
-input("Press Enter to continue...")
+def plotAll():
+    plot('data/bitcoinity_data.csv',"Time","USD","Bitcoin 2010 - Current")
+    plot('data/coindesk-ETH-close_data-2015-08-31_2017-05-22.csv',"Time","USD","ETH 2015 - Current")
+
+def plot(input, xaxis, yaxis, title):
+    df_coindesk = pd.read_csv(input)
+    ax = df_coindesk.plot(style=['o', 'rx'])
+    ax.set_xlabel(xaxis)
+    ax.set_ylabel(yaxis)
+    plt.title(title)
+    plt.show()
